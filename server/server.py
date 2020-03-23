dc08909f17c8a5dbedbcde6e7770bf7ac554939d
Nathan Merrill <mathiscool3000@gmail.com>
2020-03-23 21:38:03 +0000

    Add sprint

--- server/server.py
+++ server/server.py
@@ -1,8 +1,20 @@
 from flask import Flask, send_file, request
 from flask_socketio import SocketIO, emit
 
+from datetime import datetime
+
 app = Flask(__name__)
 sio = SocketIO(app)
+
+def sprint(tag, message, timestamp=True):
+    out = '['
+    out += tag
+    if timestamp:
+        out += ' '
+        out += datetime.now().strftime("%d-%b-%Y %-I:%M:%S %p")
+    out += '] '
+    out += message
+    print(out)
 
 @app.route('/')
 @app.route('/<path:path>')
@@ -11,11 +23,11 @@ def index(path = 'index.html'):
 
 @sio.on('connect')
 def connect():
-    print(f'[CONNECT] {request.sid}')
+    sprint('CONNECT', request.sid)
 
 @sio.on('disconnect')
 def disconnect():
-    print(f'[DISCONNECT] {request.sid}')
+    sprint('DISCONNECT', request.sid)
 
 if __name__ == '__main__':
     sio.run(app, port=4000)

a8b218ff04ed3636f9e2c5e41663227c6e9845f8
Nathan Merrill <mathiscool3000@gmail.com>
2020-03-23 20:55:17 +0000

    Add socket connection on client

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3 -u
-import eventlet
-eventlet.monkey_patch()
+
+# import eventlet
+# eventlet.monkey_patch()
 
 from flask import Flask, send_file, request
 from flask_socketio import SocketIO, emit
@@ -13,5 +14,18 @@ sio = SocketIO(app)
 def index(path = 'index.html'):
     return send_file(f'../public/{path}')
 
+@sio.on('connect')
+def connect():
+    print(f'[CONNECT] {request.sid}')
+
+@sio.on('disconnect')
+def disconnect():
+    print(f'[DISCONNECT] {request.sid}')
+
+@sio.on('ping')
+def ping(data):
+    print(f'[PING] {data}')
+
 if __name__ == '__main__':
-    app.run(port=4000)
+    # app.run(port=4000)
+    sio.run(app, port=4000)

64db2a3988a91e8af71ea10c641944b623c965d2
Nathan Merrill <mathiscool3000@gmail.com>
2020-04-04 18:51:15 +0000

    Pre babel

--- public/game.js
+++ public/game.js
@@ -1,12 +1,41 @@
 let socket = io.connect('http://localhost:4000');
 
-// ========== FUNCTIONS ==========
-// ========== START ==========
+// ========== THREE.JS SETUP ==========
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// ========== FUNCTIONS ==========
+function keyDown(event) {
+    let key = keyNames[event.which];
+    if (!inputs['keyboard'].includes(key)) {
+        inputs['keyboard'].push(key);
+    }
+}
+
+function keyUp(event) {
+    let key = keyNames[event.which];
+    inputs['keyboard'] = inputs['keyboard'].filter(item => item !== key);
+}
+
+// ========== START ==========
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 5;
+
+let inputs = {
+    'keyboard': [],
+    'mouseX': 0,
+    'mouseY': 0
+};
+
+document.addEventListener('keydown', keyDown, false);
+document.addEventListener('keyup', keyUp, false);
+// document.addEventListener('mousemove', mouseMove, false);
 
 function update() {
     requestAnimationFrame(update);
--- server/exil.py
+++ server/exil.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3 -u
 from flask import Flask, send_file, request
 from flask_socketio import SocketIO, emit
+from threading import Thread, RLock
 from datetime import datetime
 
 from config import DevelopmentConfig
@@ -11,6 +12,18 @@ sio = SocketIO(app, cors_allowed_origins='*')
 
 PORT = 4000
 
+class Player:
+    def __init__(self, sid, x, y, z):
+        self.sid = sid
+        self.x = x
+        self.y = y
+        self.z = z
+        self.inputs = {
+            'keyboard': [],
+            'mouseX': 0,
+            'mouseY': 0
+        }
+
 @app.route('/')
 @app.route('/<path:path>')
 def get(path='index.html'):
@@ -19,10 +32,16 @@ def get(path='index.html'):
 @sio.on('connect')
 def connect():
     sprint('connect', f'{request.sid} {request.remote_addr}')
+    playersLock.acquire()
+    players[request.sid] = Player(request.sid, 0, 0, 0)
+    playersLock.release()
 
 @sio.on('disconnect')
 def disconnect():
     sprint('disconnect', f'{request.sid} {request.remote_addr}')
+    playersLock.acquire()
+    players.pop(request.sid)
+    playersLock.release()
 
 def sprint(tag, text, timestamp=True):
     out = f'[{tag.upper()}'
@@ -31,8 +50,23 @@ def sprint(tag, text, timestamp=True):
     out += f'] {text}'
     print(out)
 
+def runGameLoop():
+    while True:
+        playersLock.acquire()
+        for sid in players:
+            # print(f'Running game loop on {sid}')
+            pass
+        playersLock.release()
+
 if __name__ == '__main__':
     sprint('server', 'Initializing...')
+    sprint('server', 'Initializing variables...')
     players = {}
+    playersLock = RLock()
+
+    sprint('server', 'Initializing game loop...')
+    gameLoopThread = Thread(target=runGameLoop)
+    gameLoopThread.start()
+
     sprint('server', f'Starting web server on port {PORT}')
     sio.run(app, host='0.0.0.0', port=PORT)
